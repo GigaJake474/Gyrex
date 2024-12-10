b47d616a7978cbd5373fbaeb49312dca9cf3227e
GigaJake474 <jacoseke474@gmail.com>
2024-12-10 13:45:14 -0500

    Tested get function and fixed bugs

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-#Variables
+# Variables
 item = "item"
 
-#Functions
+# Functions
 def get():
     print("You picked up " + item)
+
+# This is here to test get function
+item = "a key."
+user_input = input("test get")
+if user_input == "get":
+    get()
