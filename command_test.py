9ac7dbbed694b4cf9b18f9df6ff01f9c772203b1
GigaJake474 <jacoseke474@gmail.com>
2024-12-10 13:52:18 -0500

    Created and edited commands.py and command_test.py

---
+++ command_test.py
@@ -0,0 +1,8 @@
+#Modules
+import commands
+
+# This is here to test get function
+item = "a key."
+user_input = input("test get")
+if user_input == "get":
+    commands.get()
--- commands.py
+++ commands.py
@@ -4,9 +4,3 @@ item = "item"
 # Functions
 def get():
     print("You picked up " + item)
-
-# This is here to test get function
-item = "a key."
-user_input = input("test get")
-if user_input == "get":
-    get()
